d3b7c7e1b823dfcdafbdf41ef3aad2e33e7c5058
Vil Gilyazetdinov <sith1988@gmail.com>
2016-03-24 10:58:32 +0500

    Сделал обработку нажатий в одном обработчике, изменил иконку приложения.

--- app/src/main/java/com/example/gilyazetdinovvr/trafficlight/MainActivity.java
+++ app/src/main/java/com/example/gilyazetdinovvr/trafficlight/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.gilyazetdinovvr.trafficlight;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private TextView mColorTextView;
     private RelativeLayout mRelativeLayout;
@@ -27,56 +22,36 @@ public class MainActivity extends AppCompatActivity {
 
         mColorTextView = (TextView)findViewById(R.id.mTextViewColor);
         mRelativeLayout = (RelativeLayout)findViewById(R.id.mRelativeLayout);
-        Button RedButton = (Button)findViewById(R.id.buttonRed);
-        RedButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Button mGreenButton = (Button)findViewById(R.id.buttonGreen);
+        Button mYellowButton = (Button)findViewById(R.id.buttonYellow);
+        Button mRedButton = (Button)findViewById(R.id.buttonRed);
+
+        mGreenButton.setOnClickListener(this);
+        mYellowButton.setOnClickListener(this);
+        mRedButton.setOnClickListener(this);
+
+
+
+
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonGreen:
+                mColorTextView.setText(R.string.color_green);
+                mRelativeLayout.setBackgroundResource(R.color.colorGreen);
+                break;
+            case R.id.buttonYellow:
+                mColorTextView.setText(R.string.color_yellow);
+                mRelativeLayout.setBackgroundResource(R.color.colorYellow);
+                break;
+            case R.id.buttonRed:
                 mColorTextView.setText(R.string.color_red);
-                mRelativeLayout.setBackgroundColor(getResources().getColor(R.color.colorRed));
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                mRelativeLayout.setBackgroundResource(R.color.colorRed);
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
-    public void onGreenButtonClick(View view) {
-        mColorTextView.setText(R.string.color_green);
-        mRelativeLayout.setBackgroundColor(getResources().getColor(R.color.colorGreen));
 
-    }
-
-    public void onYellowButtonClick(View view) {
-        mColorTextView.setText(R.string.color_yellow);
-        mRelativeLayout.setBackgroundResource(R.color.colorYellow);
-    }
 }
